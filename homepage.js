8eb7b8ed5cd20287129105dbdbe18473ae8abef1
Msteimel <mitch.steimel@gmail.com>
2017-02-22 15:44:46 -0600

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -3,6 +3,13 @@ $(function() {
 /**
  *  Scroll window to an element
  */
+$('a[href^="#home"]').click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+}); 
+    
 $('a[href^="#story"]').click(function(e) {
     e.preventDefault();
     $('html, body').animate({
