2c1bf6bdc50a95f3d69a22ec1c33030df65d3e6a
Mitchell Steimel <msteimel@trilixgroup.com>
2019-03-05 11:08:22 -0600

    SCSS and SuperHI project commit

---
+++ api-projects/project-1/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<!--[if IE 9 ]>   <html class="ie9" lang="en"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Font Awesome -->
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+  <!-- End Font Awesome -->
+  <!-- Fonts-->
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display|Work+Sans" rel="stylesheet">
+ <title>SuperHi JS for Designers Project 1</title>
+
+
+  <link rel="stylesheet" href="/css/api-project-1.css">
+</head>
+
+<body>
+
+<div class="grid">
+	<div class="col-10 offset-1">
+		<section>
+			<div class="circle"></div>
+			<h1>Mitch Steimel is...</h1>
+			<h2>A Des Moines Front-end Web Dev</h2>
+		</section>
+		
+		<footer>
+			<!-- <span class="prev"><i class="fas fa-arrow-left"></i></span> -->
+			<span class="random"><i class="fas fa-random"></i></span>
+			<!-- <span class="next"><i class="fas fa-arrow-right"></i></span> -->
+		</footer>
+	</div>
+</div>
+
+<!-- <script type="text/javascript" src="api-projects/project-1/javascript/slide.js"></script> -->
+</body>
+</html>
+
+
